62863efc44c910441c40c63630d07cf449464b47
mehdi-norouzi <mehdeenoroozi@gmail.com>
2023-12-10 18:22:38 +0330

    day2 part2 rust

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -1,31 +1,38 @@
 use std::fs;
 
+struct MaxValues<'a> {
+    _color: &'a str,
+    count: u32,
+}
+
 fn main() {
+
     let f = fs::read_to_string("puzzle").expect("no file found");
     let lines = f.lines();
     let mut sum = 0;
-    'next_game_id: for line in lines {
-        let (game_id_str, game) = line.split_once(": ").unwrap();
-        let game_id_iter: Vec<&str> = game_id_str.split(' ').collect();
-        let game_id = game_id_iter[1].parse::<u32>().unwrap();
+    for line in lines {
+        let mut max: [MaxValues; 3] = [
+            MaxValues {_color: "red", count: 0},
+            MaxValues {_color: "green", count: 0},
+            MaxValues {_color: "blue", count: 0},
+        ];
+        let (_game_id_str, game) = line.split_once(": ").unwrap();
         let rounds: Vec<&str> = game.split("; ").collect();
         for round in rounds {
             let rolls: Vec<&str> = round.split(", ").collect();
             for roll in rolls {
                 let (count, color) = roll.split_once(" ").unwrap();
                 let count: u32 = count.parse::<u32>().unwrap();
-                let game_valid: bool = match color {
-                    "blue" => count <= 14,
-                    "red" => count <= 12,
-                    "green" => count <= 13,
-                    _ => false,
-                };
-                if !game_valid {
-                    continue 'next_game_id;
-                } 
+                match color {
+                    "red" => {max[0].count = std::cmp::max(count, max[0].count);}
+                    "green" => {max[1].count = std::cmp::max(count, max[1].count);}
+                    "blue" => {max[2].count = std::cmp::max(count, max[2].count);}
+                    _ => todo!()
+                }
             }
         }
-        sum += game_id;
+        let power = max[0].count * max[1].count * max[2].count;
+        sum += power;
     }
     println!("sum = {}", sum);
 }
